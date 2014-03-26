f806e0f44eade4797286dea110a72f21f369bfa0
Martin Hagerup <martin.hagerup@gmail.com>
2014-03-26 19:27:03 +0100

    added auth with pw encrytion

--- run.py
+++ run.py
@@ -19,11 +19,26 @@ class BCryptAuth(BasicAuth):
             return account and \
                 bcrypt.hashpw(password, account['password']) == account['password']
 
+def post_get_callback(resource, request, payload):
+    print('A GET on the', resource, 'was just performed!', payload)
+
+def pre_insert_accounts(items):
+    #Hooks the passwords and encrypts them before storage
+    for item in items:
+        password = item['password']
+        item['password'] = bcrypt.hashpw(password, bcrypt.gensalt())
+
 if __name__ == '__main__':
     #app = Eve(auth=MyBasicAuth)
     app = Eve(auth=BCryptAuth)
+
     #app = Eve()
     Bootstrap(app)
+
+    #Hooks
+    #app.on_post_GET += post_get_callback
+    app.on_insert_accounts+=pre_insert_accounts
+
     app.register_blueprint(eve_docs, url_prefix='/docs')
 
     app.run(host='0.0.0.0', debug=True)
